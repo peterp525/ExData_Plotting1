8d53ab21ddd2c2b9fec987eed86356b18157d657
peterp525 <p.puniani1@gmail.com>
2016-12-31 17:29:03 -0500

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,21 @@
+## read data
+data <- read.csv("C:/Users/Peter/Documents/Coursera/JH_DS/Exploratory Data Analysis/household_power_consumption.txt", header=T, sep=';', na.strings="?",nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+
+# convert the date variable to Date class
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+# Subset the data
+data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+# Convert dates and times
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+
+# Plot 1
+attach(data)
+hist(Global_active_power, main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)", col = "Red")
+
+# Save file
+dev.copy(png, file = "plot1.png", height = 480, width = 480)
+dev.off()
+detach(data)
---
+++ plot2.R
@@ -0,0 +1,20 @@
+## read data
+data <- read.csv("C:/Users/Peter/Documents/Coursera/JH_DS/Exploratory Data Analysis/household_power_consumption.txt", header=T, sep=';', na.strings="?",nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+
+# convert the date variable to Date class
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+# Subset the data
+data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+# Convert dates and times
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+
+# Plot 2
+data$datetime <- as.POSIXct(data$datetime)
+attach(data)
+plot(Global_active_power ~ datetime, type = "l",
+     ylab = "Global Active Power (kilowatts)", xlab = "")
+dev.copy(png, file = "plot2.png", height = 480, width = 480)
+dev.off()
+detach(data)
